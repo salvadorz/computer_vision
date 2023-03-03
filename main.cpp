b5d0bf1d4c6af1065a494671520ce8bd62a2701d
Salvador Zendejas <salvador.zendejas@gmail.com>
2023-03-03 15:15:23 -0600

    Added main.cpp to validate c++ compiler

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main( int argc, char** argv )
+{
+    std::cout << "Hello GitHub" << std::endl;
+ 
+    return 0;
+}
