534e3cc299ded3a466ee560b08bf3814027aed82
Salvador Zendejas <salvador.zendejas@gmail.com>
2023-03-04 18:43:27 -0600

    Improved scalable structure
    - Separate biniries
    - Output directory out of build CMAKE_BINARY_DIR based on target

--- src/bin/simple_image.cpp
+++ src/bin/simple_image.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 int main( int argc, char** argv )
 {
     std::cout << "Hello Lenna" << std::endl;
-    std::string img = "../img/lenna.jpg";
+    std::string img = "../../img/lenna.jpg";
     Mat srcImage = imread(img);
     if (!srcImage.data) {
         return EXIT_FAILURE;
