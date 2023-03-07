5cc94ac11c8aec046f86d38cc0b26db4ae132310
Salvador Zendejas <salvador.zendejas@gmail.com>
2023-03-06 22:26:35 -0600

    handling the case when the `imread` has no data.

--- src/bin/lut_transform.cpp
+++ src/bin/lut_transform.cpp
@@ -2,14 +2,15 @@
 
 #include <opencv2/opencv.hpp>
 
+/// @brief Helper Function to apply a transform function tu be used by the LUT
+/// @tparam Callable is the handle to any callable functions, functors with a call-operator
+/// @param mat_tf array to store the function implementation received by
+/// @param f function
 template<typename Callable>
-void transform_function (cv::Mat& mat_tf,Callable f)//std::function<uchar(int)> f
+void transform_function (cv::Mat& mat_tf,Callable f)
 {
     for (int i = 0; i < mat_tf.cols; ++i)
-    {
         mat_tf.at<uchar>(i) = f(i);
-    }
-    
 }
 
 int main( int argc, char** argv )
@@ -18,24 +19,24 @@ int main( int argc, char** argv )
     std::cout << "Hello Lenna" << std::endl;
     std::string str_img = "../../img/lenna.jpg";
     cv::Mat mat_Image = cv::imread(str_img);
-    std::vector<cv::Mat> lut_transform;
-    std::vector<cv::Mat> Img_transform;
-    
-    for (size_t i = 0; i < 3u; ++i)
-    {
-        lut_transform.push_back(cv::Mat(1,256,CV_8U));
-        Img_transform.push_back(mat_Image.clone());
-    }
-    
-    transform_function(lut_transform[0], [](const int x) -> uchar { return 255 - x; } );
-    transform_function(lut_transform[1], [](const int x) -> uchar { return pow((float)x * 255,(1/2.0)); } );
-    transform_function(lut_transform[2], [](const int x) -> uchar { return pow((float)x, 3.0) / (255*255); } );
-
-    cv::LUT(mat_Image, lut_transform[0], Img_transform[0]);
-    cv::LUT(mat_Image, lut_transform[1], Img_transform[1]);
-    cv::LUT(mat_Image, lut_transform[2], Img_transform[2]);
 
     if (mat_Image.data) {
+        std::vector<cv::Mat> lut_transform;
+        std::vector<cv::Mat> Img_transform;
+        
+        for (size_t i = 0; i < 3u; ++i)
+        {
+            lut_transform.push_back(cv::Mat(1,256,CV_8U));
+            Img_transform.push_back(mat_Image.clone());
+        }
+        
+        transform_function(lut_transform[0], [](const int x) -> uchar { return 255 - x; } );
+        transform_function(lut_transform[1], [](const int x) -> uchar { return pow((float)x * 255,(1/2.0)); } );
+        transform_function(lut_transform[2], [](const int x) -> uchar { return pow((float)x, 3.0) / (255*255); } );
+
+        cv::LUT(mat_Image, lut_transform[0], Img_transform[0]);
+        cv::LUT(mat_Image, lut_transform[1], Img_transform[1]);
+        cv::LUT(mat_Image, lut_transform[2], Img_transform[2]);
         
         cv::namedWindow("Lenna Image", cv::WINDOW_AUTOSIZE);
         cv::imshow("Lenna Image", mat_Image);
@@ -52,7 +53,7 @@ int main( int argc, char** argv )
     else
         ret = EXIT_FAILURE;
 
-    cv::waitKey(0);
+    cv::waitKey(4000);
 
     return ret;
 }
